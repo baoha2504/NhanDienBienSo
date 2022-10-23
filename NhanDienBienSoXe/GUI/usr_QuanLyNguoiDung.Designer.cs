a0f3b83c1603531982d5060c10b8f9a53499b4a2
baoha2504 <hacongquoclun1@gmail.com>
2022-10-23 23:51:47 +0700

    first commit

---
+++ NhanDienBienSoXe/GUI/usr_QuanLyNguoiDung.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace NhanDienBienSoXe.GUI
+{
+    partial class usr_QuanLyNguoiDung
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // usr_QuanLyNguoiDung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "usr_QuanLyNguoiDung";
+            this.Size = new System.Drawing.Size(1169, 685);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
